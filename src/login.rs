6ce4a3b3b812f810118378f9046ab0a315c2676c
Dieg0407 <diego.pastor.0407@gmail.com>
2024-04-16 21:56:57 -0500

    chore: added tests

--- src/login.rs
+++ src/login.rs
@@ -2,14 +2,33 @@ use rand::Rng;
 use base64::{prelude::BASE64_STANDARD as b64, Engine};
 
 pub fn login() -> i32 {
+    println!("Encoded salt: {}", generate_salt());
+    0
+}
+
+fn generate_salt() -> String {
     let salt: String = rand::thread_rng()
         .sample_iter(rand::distributions::Alphanumeric)
         .take(128)
         .map(char::from)
         .collect();
 
-    let salt = b64.encode(salt.as_bytes());
-    println!("Encoded salt: {}", salt);
+    b64.encode(salt.as_bytes())
+}
 
-    0
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use base64::{prelude::BASE64_STANDARD as b64, Engine};
+
+    #[test]
+    fn should_generate_an_alphanumerical_salt() {
+        let salt = generate_salt();
+        let bytes = b64.decode(salt).unwrap();
+        let decoded = String::from_utf8(bytes).unwrap();
+
+        for c in decoded.chars() {
+            assert!(c.is_alphanumeric());
+        }
+    }
 }
