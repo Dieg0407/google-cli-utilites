8a1d2df30cb5773d00f20756b5050068efe98c95
Dieg0407 <diego.pastor.0407@gmail.com>
2024-06-19 22:07:11 -0500

    feat: initialized application

---
+++ cli/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(libs.guava)
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "com.dieg0407.GoogleCliApp"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ cli/src/main/java/com/dieg0407/GoogleCliApp.java
@@ -0,0 +1,9 @@
+package com.dieg0407;
+
+import static java.lang.System.err;
+
+public class GoogleCliApp {
+    public static void main(String[] args) {
+        err.println("This is the google cli app!");
+    }
+}
